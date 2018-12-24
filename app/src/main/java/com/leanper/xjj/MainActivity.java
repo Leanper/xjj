59c926dd9776b57e4c7d941e2bd58934b42dabe3
Leanper <15116920121@163.com>
2018-12-24 17:35:36 +0800

    第二个版本

---
+++ app/src/main/java/com/leanper/xjj/MainActivity.java
@@ -0,0 +1,23 @@
+package com.leanper.xjj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @Title:  MainActivity 
+ 
+ * @Description:    
+ * @author:  Leanper
+ * @date:   2018/12/24 17:22   
+ 
+ */  
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
